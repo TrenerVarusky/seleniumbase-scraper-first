1972b2ac6458cf20d6178dc676e149c6cc8d45ce
Trener Varusky <adox1899@gmail.com>
2025-06-27 10:07:26 +0200

    Zaktualizowano scraper i dodano requirements.txt

--- my_first_scraper.py
+++ my_first_scraper.py
@@ -1,2 +1,6 @@
-print("Działa!")
+from seleniumbase import BaseCase
 
+class MyFirstScraper(BaseCase):
+    def test_scrape_example(self):
+        self.open("https://example.com")
+        print("Tytuł strony:", self.get_title())
